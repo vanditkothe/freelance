31dade5ce5d51a25a9f816e32c94dea44c1873ab
vandit kothe <vanditkothe@vandits-MacBook-Air.local>
2025-08-10 18:07:16 +0530

    Update API URLs to use deployed backend

--- freelance-app-frontend/src/services/apiConnector.js
+++ freelance-app-frontend/src/services/apiConnector.js
@@ -2,24 +2,11 @@ import axiosInstance from "../utils/axios";
 
 export const apiConnector = async (method, url, data = {}, options = {}) => {
   try {
-    const token = localStorage.getItem("token");
-
-    const { headers: customHeaders = {}, ...restOptions } = options;
-
-    const headers = {
-      ...customHeaders,
-      ...(token && !customHeaders.Authorization
-        ? { Authorization: `Bearer ${token}` }
-        : {}),
-    };
-
     const config = {
       method,
       url,
       data,
-      withCredentials: true,
-      headers,
-      ...restOptions,
+      ...options,
     };
 
     const response = await axiosInstance(config);
